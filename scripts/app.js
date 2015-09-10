aa3a94d789f9f22331e20fb36521eb259c2a20de
Eric Ng <eric.ng@autodesk.com>
2015-09-10 12:23:49 -0400

    Unit 1 Lesson 3.2

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,49 @@
 angular.module('madlibs', [])
 .controller('mainCtrl', function($scope) {
-	$scope.femaleName = '{{female name}}';
+	$scope.genders = [
+		{label: 'Select gender', val: 0},
+		{label: 'male', val: 1},
+		{label: 'female', val: 2}
+	];
+	$scope.genderSpecifics = [
+		{ // default
+			name: 'madlibs',
+			subject: 'it',
+			possession: 'its',
+			object: 'it'
+		}, 
+		{ // male
+			name: 'male',
+			subject: 'he',
+			possession: 'his',
+			object: 'him'
+		},
+		{ // female
+			name: 'female',
+			subject: 'she',
+			possession: 'her',
+			object: 'her'
+		}
+	];
+	$scope.selectedGender = 0;
+
+	// variables for content
+	var _name = '';
+	$scope.name = function (newVal) {
+		return arguments.length ? (_name = newVal) : 
+			(_name.length ? _name : 
+				($scope.selectedGender === 0 ? '{{name}}' : '{{' + $scope.genderSpecifics[$scope.selectedGender].name + ' name}}')
+			);
+	}
+	$scope.subject = function () {
+		return $scope.genderSpecifics[$scope.selectedGender].subject;
+	}
+	$scope.possession = function () {
+		return $scope.genderSpecifics[$scope.selectedGender].possession;
+	}
+	$scope.object = function () {
+		return $scope.genderSpecifics[$scope.selectedGender].object;
+	}
 	$scope.jobTitle = '{{job title}}';
 	$scope.tediousTask = '{{tedious task}}';
 	$scope.celebrity = '{{celebrity}}';
